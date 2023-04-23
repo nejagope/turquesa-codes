822c378676f557119b84a5c56964db2f3ccca902
Nelson Jair González <njgp86@gmail.com>
2023-04-23 13:32:27 -0600

    Validate username pattern

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 var working = false;
 function login(){
-    console.log('login')
-   
     if (working) return;
     working = true;
     var $this = $(".login"),
     $state = $this.find('button > .state');
     var username = $("#username").val();
     var password = $("#password").val();
+
+    if (!username || !password){
+      working = false;
+      return;
+    }else{
+      const myRe = /^\d{1,2}([a-z]|([A-Z]))$/g;
+      var pruebaDirección = myRe.test(username);
+      console.log(pruebaDirección);
+      if (!pruebaDirección){
+        console.log('no pasó la prueba')
+        $this.addClass('bad');          
+          $(".spinner").hide();          
+          $state.html('Debe ingresar el número de casa seguido de la manzana sin espacios. (Por ejemplo: 5A, 16H, 7E)');
+          setTimeout(function() {
+            $state.html('Log in');
+            $this.removeClass('bad ok loading');                  
+            $(".spinner").show();            
+            working = false;            
+          }, 4000);
+          return;
+      }  
+    }
     
     $this.addClass('loading');
     $state.html('Authenticating');
